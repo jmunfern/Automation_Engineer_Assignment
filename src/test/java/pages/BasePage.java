cbc1606b85d6aa2a8168f5d8695e3942ef1ceb57
jmunfer <jorgeluis11.munozfernandez@emeal.nttdata.com>
2025-04-09 12:22:36 -0500

    Update pages\BasePage.java

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,6 +18,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BasePage {
     
     protected static WebDriver driver;
+     private static Actions action;
     
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
  
@@ -37,7 +39,13 @@ public class BasePage {
     public static void navigateTo(String url) {
         driver.get(url);
     }
-    // Cierra el navegadoir
+
+    // Encuentra el elemento de enlace en la página web usando el texto del enlace y hace clic en él
+    public void goToLinkText(String linkText){
+        driver.findElement(By.linkText(linkText)).click();;
+    }
+    
+    // Cierra el navegador
     public static void closeBrowser(){
         driver.quit();
     }
@@ -65,5 +73,63 @@ public class BasePage {
         Select dropdown = new Select(Find(locator));
         dropdown.selectByIndex(index);
     }
+    
+   // Acciones
+
+    // Pasar el cursor sobre el elemento
+    public void hoverOverElement(String locator) {
+        // Mueve el cursor al elemento encontrado por el localizador
+        action.moveToElement(Find(locator));
+    }
+
+    // Doble clic
+    public void doubleClick(String locator) {
+        // Realiza un doble clic en el elemento encontrado por el localizador
+        action.doubleClick(Find(locator));
+    }
+
+    // Clic derecho
+    public void rightClick(String locator) {
+        // Realiza un clic derecho en el elemento encontrado por el localizador
+        action.contextClick(Find(locator));
+    }
+
+    // iFrame y PopUps
+    public void switchToIframe(int iFrameIndex) {
+        // Cambia al iFrame especificado por el índice
+        driver.switchTo().frame(iFrameIndex);
+    }
+
+    public void switchToParentFrame() {
+        // Cambia al marco padre
+        driver.switchTo().parentFrame();
+    }
+
+    public void dismissAlert() {
+        // Desestima la alerta
+        driver.switchTo().alert().dismiss();
+    }
+
+    public String textFromElement(String locator) {
+        // Obtiene el texto del elemento encontrado por el localizador
+        return Find(locator).getText();
+    }
+
+    // Elementos booleanos
+    public boolean elementEnabled(String locator) {
+        // Verifica si el elemento encontrado por el localizador está habilitado para ser clicable
+        return Find(locator).isEnabled();
+    }
+
+    public boolean elementIsDisplayed(String locator) {
+        // Verifica si el elemento encontrado por el localizador está visible en la página
+        return Find(locator).isDisplayed();
+    }
+
+    public boolean elementIsSelected(String locator) {
+        // Verifica si el elemento encontrado por el localizador (como un checkbox) está seleccionado
+        return Find(locator).isSelected();
+    }
+
     
 }
